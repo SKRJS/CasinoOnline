730222cf23206702579bdf66ad4a0f54ae849831
SK AYUB AL WAHID <sk@SKs-MacBook-Air.local>
2024-10-05 16:24:37 -0700

    CasinoOnline

---
+++ src/component/Pages/SideArea.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import '../../App.css';
+
+const SideArea = () => {
+    return (
+        <div className="content-link-page">
+              <div >
+                 <h4>Content on page </h4>
+                 <ul>
+                   <li><a href="/">Online Casinos</a></li>
+                   <li><a href="/New_Online_Casinos"> New Online Casinos</a></li>
+                   <li><a href="/Bonuses_CASINO"> Casino Bonus</a></li>
+                   <li><a href="/No_Deposit"> No Deposit Bonus</a></li>
+                   <li><a href="/mobile">Mobile Casino</a></li>
+                   <li><a href="/slot">Best Slots Casino Game</a></li>
+                   <li><a href="/FAST_WITHDRAWAL_CASINO">Fast Withdrawal Casinos</a></li>
+                 </ul>
+               </div>
+        </div>
+    );
+};
+
+export default SideArea;
